388e89f55c7480c8069dc9749f814d0658ad786b
crazypoo <273277355@qq.com>
2014-01-15 11:31:02 +0800

    M7_step
    運用M7處理器計步

--- step/ViewController.m
+++ step/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
+@property (nonatomic, strong) CMStepCounter *stepCounter;
+@property (nonatomic, strong) CMMotionActivityManager *activityManager;
+@property (nonatomic, strong) NSOperationQueue *operationQueue;
+@property (nonatomic, weak) IBOutlet UILabel *stepsLabel;
+@property (nonatomic, weak) IBOutlet UILabel *statusLabel;
+@property (nonatomic, weak) IBOutlet UILabel *confidenceLabel;
 
 @end
 
@@ -18,8 +25,126 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    if (!([CMStepCounter isStepCountingAvailable] || [CMMotionActivityManager isActivityAvailable])) {
+        
+        NSString *msg = @"StepCounter and MotionActivityManager are not available. These classes need M7 coprocessor, so this sample works only on iPhone5s.";
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Not Supported"
+                                                        message:msg
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        
+        return;
+    }
+    __weak ViewController *weakSelf = self;
+    
+    self.operationQueue = [[NSOperationQueue alloc] init];
+    
+    //更新label
+    if ([CMStepCounter isStepCountingAvailable]) {
+        
+        self.stepCounter = [[CMStepCounter alloc] init];
+        
+        [self.stepCounter startStepCountingUpdatesToQueue:self.operationQueue
+                                                 updateOn:1
+                                              withHandler:
+         ^(NSInteger numberOfSteps, NSDate *timestamp, NSError *error) {
+             
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 
+                 if (error) {
+                     UIAlertView *error = [[UIAlertView alloc] initWithTitle:@"Opps!" message:@"error" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                     [error show];
+                 }
+                 else {
+                     
+                     NSString *text = [NSString stringWithFormat:@"Steps: %ld", (long)numberOfSteps];
+                     
+                     weakSelf.stepsLabel.text = text;
+                 }
+             });
+         }];
+    }
+    
+//開波
+    if ([CMMotionActivityManager isActivityAvailable]) {
+        
+        self.activityManager = [[CMMotionActivityManager alloc] init];
+        
+        [self.activityManager startActivityUpdatesToQueue:self.operationQueue
+                                              withHandler:
+         ^(CMMotionActivity *activity) {
+             
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 
+                 NSString *status = [weakSelf statusForActivity:activity];
+                 NSString *confidence = [weakSelf stringFromConfidence:activity.confidence];
+                 
+                 weakSelf.statusLabel.text = [NSString stringWithFormat:@"Status: %@", status];
+                 weakSelf.confidenceLabel.text = [NSString stringWithFormat:@"Confidence: %@", confidence];
+             });
+         }];
+    }
 }
-
+- (NSString *)statusForActivity:(CMMotionActivity *)activity {
+    
+    NSMutableString *status = @"".mutableCopy;
+    
+    if (activity.stationary) {
+        
+        [status appendString:@"not moving"];
+    }
+    
+    if (activity.walking) {
+        
+        if (status.length) [status appendString:@", "];
+        
+        [status appendString:@"on a walking person"];
+    }
+    
+    if (activity.running) {
+        
+        if (status.length) [status appendString:@", "];
+        
+        [status appendString:@"on a running person"];
+    }
+    
+    if (activity.automotive) {
+        
+        if (status.length) [status appendString:@", "];
+        
+        [status appendString:@"in a vehicle"];
+    }
+    
+    if (activity.unknown || !status.length) {
+        
+        [status appendString:@"unknown"];
+    }
+    
+    return status;
+}
+- (NSString *)stringFromConfidence:(CMMotionActivityConfidence)confidence {
+    
+    switch (confidence) {
+            
+        case CMMotionActivityConfidenceLow:
+            
+            return @"Low";
+            
+        case CMMotionActivityConfidenceMedium:
+            
+            return @"Medium";
+            
+        case CMMotionActivityConfidenceHigh:
+            
+            return @"High";
+            
+        default:
+            
+            return nil;
+    }
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];

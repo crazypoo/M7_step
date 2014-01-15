f80806e3f52de1dc75b44808c9e69a586ee15cd1
crazypoo <273277355@qq.com>
2014-01-15 11:13:53 +0800

    Initial Commit

---
+++ step/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  step
+//
+//  Created by crazypoo on 1/15/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
